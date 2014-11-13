48fb62f0894a81403f6852ba56aba98ac31b9b51
Alexey Kramarenko <alexeyk13@yandex.ru>
2014-11-13 20:26:09 +0300

    STM32L0 uart full setup

--- board/stm32l0/board.c
+++ board/stm32l0/board.c
@@ -7,14 +7,16 @@
 #include "../../board.h"
 #include "stm32.h"
 #include "gpio.h"
+#include "power.h"
 
 void board_init()
 {
     __disable_irq();
+    power_init();
     gpio_set_pin(A5, true);
 }
 
 void board_reset()
 {
-    __enable_irq();
+    NVIC_SystemReset();
 }
---
+++ board/stm32l0/uart.h
@@ -0,0 +1,21 @@
+/*
+    RExOS - embedded RTOS
+    Copyright (c) 2011-2014, Alexey Kramarenko
+    All rights reserved.
+*/
+/*
+    USB DFU Flasher
+    Copyright (c) 2014, Alexey Kramarenko
+    All rights reserved.
+*/
+
+#ifndef UART_H
+#define UART_H
+
+typedef enum {
+    UART_1 = 0,
+    UART_2,
+    UART_MAX
+}UART_PORT;
+
+#endif // UART_H
